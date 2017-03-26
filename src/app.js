c04387ec5b160ce4154266e6be464141967b4a6d
oliverlai55 <oliverlai55@gmail.com>
2017-03-26 02:54:14 -0400

    Add Firebase

--- src/app.js
+++ src/app.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBCWHsJ4QKWDFE-Z9WW4o13lr6fJn6_5Fg',
+      authDomain: 'authentication-b981c.firebaseapp.com',
+      databaseURL: "https://authentication-b981c.firebaseio.com",
+      storageBucket: "authentication-b981c.appspot.com",
+      messagingSenderId: "438698945980"
+    });
+  }
+
   render() {
     return  (
       <View>
         <Header headerText="Authentication" />
-        <Text>An App!</Text>
+        <LoginForm />
       </View>
     );
   }
 }
 
 export default App;
+
+// npm install --save-dev eslint-config-rallycoding
