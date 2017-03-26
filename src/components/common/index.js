a75aaf019257bd62510f2109eca9651769ef94aa
oliverlai55 <oliverlai55@gmail.com>
2017-03-26 13:44:18 -0400

    Refactor index.js

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -2,5 +2,5 @@ export * from './Button';
 export * from './Card';
 export * from './CardSection';
 export * from './Header';
-
+export * from './Input';
 // this imports and exports everything at once
